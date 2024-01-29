5ded8a72044a31977d78c821ef6b179b192a1209
Ayoub Amzil <85901966+ayoub-amzil@users.noreply.github.com>
2024-01-29 11:00:42 +0100

    Setup variables

--- index.py
+++ index.py
@@ -5,3 +5,9 @@ import platform
 import getpass
 import pytube as pt
 from urllib.parse import urlparse
+
+app_name = 'Youtube Downloader'
+app_logo = tm.colored(' ► ', 'white', 'on_red')+tm.colored(app_name, 'red', 'on_white')
+path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')+'\\'+app_name
+valid = tm.colored(' ✔ ', 'white', 'on_green')
+not_valid = tm.colored(' ✘ ', 'white', 'on_red')
